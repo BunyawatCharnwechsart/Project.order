9e8a4a00131ab4845bc706953a587d449ff1a051
unknown <bunyawat2149m@gmail.com>
2025-03-04 16:17:03 +0700

    order0.2

--- component/sidebar.js
+++ component/sidebar.js
@@ -2,22 +2,22 @@
 class sidebar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <aside>
+       <aside>
         <div class="logo">
             <img src="/JPG/logomormilk.png" alt="Mormilk">
         </div>
         <nav>
             <div class="icon">
-                <a href="/index.html"><img src="/JPG/Drinkicon.png"alt="Drinkicon"></a>
+                <a href="/index.html"><img src="/JPG/drinkicon.png"alt="Drinkicon"></a>
             </div>
             <div class="icon">
                 <a href="/main/menudessert.html"><img src="/JPG/Desserticon.png"alt="Desserticon"></a>
             </div>
             <div class="icon">
-                <a href="/main/menufood.html"><img src="/JPG/foodicon.png.png"alt="Foodicon"></a>
+                <a href="/main/menufood.html"><img src="/JPG/foodicon.png"alt="Foodicon"></a>
             </div>
             <div class="icon">
-                <a href="/main/menucart.html><img src="/JPG/carticon.png"alt="Carticon"></a>
+                <a href="/main/menucart.html"><img src="/JPG/carticon.png" alt="Carticon"></a>
             </div>
             <div class="icon">
                 <a href="/main/menubill.html"><img src="/JPG/bill.png"alt="Billicon"></a>
